9c4bebce5c20b1f883e2067d267d136f437dd914
kevinetl01 <kevinetl01@gmail.com>
2025-07-09 17:20:00 -0500

    Crud roles Done

--- courses_pages/users/api/views.py
+++ courses_pages/users/api/views.py
@@ -13,8 +13,22 @@ from ..models import Role
 #serializers
 from .serializers import RoleSerializer
 
+class IsAdminRole(permissions.BasePermission):
+    """
+        Permission to only allow admins to access the view.
+    """
+
+    message = "You need a higher level of permission."
+
+    def has_permission(self, request, view):
+        user = request.user
+
+        if not user or not user.is_authenticated:
+            return False
+
+        return user.roles.filter(name__iexact='admin').exists()
 
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminRole]
