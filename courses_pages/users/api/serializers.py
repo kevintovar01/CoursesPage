294d4cad8f484b82d53d02cfd25f4dca3e9b6c13
kevinetl01 <kevinetl01@gmail.com>
2025-07-09 19:30:42 -0500

    id users

--- courses_pages/users/api/serializers.py
+++ courses_pages/users/api/serializers.py
@@ -13,19 +13,20 @@ class CountrySerializer(serializers.ModelSerializer):
         model = Country
         fields = '__all__'
 
+
+class RegistrationSerializer(serializers.ModelSerializer):
+
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'born_date', 'country', 'password', 'password2')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id','email', 'first_name', 'last_name', 'born_date', 'country', 'password')
+        fields = ('id','email', 'first_name', 'last_name', 'born_date', 'country')
         extra_kwargs = {'password': {'write_only': True}}
-
-        def create(self, validated_data):
-            # 1) Extraemos el password plano
-            raw_pw = validated_data.pop('password')
-            # 2) Creamos el usuario sin el password
-            user = User.objects.create(**validated_data)
-            # 3) Le hacemos hashing al password y lo guardamos
-            user.set_password(raw_pw)
-            user.save()
-            return user
 
