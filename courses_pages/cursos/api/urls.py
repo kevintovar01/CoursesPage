f9b7c623e5661190772676731e753886b1638841
Jhojan98 <jhojanramirez75@gmail.com>
2025-07-09 19:06:08 -0500

    Urls mejoradas

--- courses_pages/cursos/api/urls.py
+++ courses_pages/cursos/api/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CursoViewSet, ModuloViewSet, MaterialViewSet, RealizaViewSet
+from .views import (
+    CursoList, CursoDetail, 
+    ModuloViewSet, MaterialViewSet, RealizaViewSet
+)
 
 router = DefaultRouter()
-router.register(r'cursos', CursoViewSet)
 router.register(r'modulos', ModuloViewSet)
 router.register(r'materiales', MaterialViewSet)
 router.register(r'realiza', RealizaViewSet)
 
 urlpatterns = [
+    path('cursos/', CursoList.as_view(), name='curso-list'),
+    path('cursos/<int:pk>/', CursoDetail.as_view(), name='curso-detail'),
     path('', include(router.urls)),
 ]
--- courses_pages/cursos/api/views.py
+++ courses_pages/cursos/api/views.py
@@ -1,13 +1,57 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from ..models import Curso, Modulo, Material, Realiza
 from .serializers import CursoSerializer, ModuloSerializer, MaterialSerializer, RealizaSerializer
 
 # Create your views here.
 
-class CursoViewSet(viewsets.ModelViewSet):
-    queryset = Curso.objects.all()
-    serializer_class = CursoSerializer
+# Usando APIView para control total sobre el modelo Curso
+class CursoList(APIView):
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CursoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Curso.objects.get(pk=pk)
+        except Curso.DoesNotExist:
+            return None
+    
+    def get(self, request, pk):
+        curso = self.get_object(pk)
+        if curso is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = CursoSerializer(curso)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        curso = self.get_object(pk)
+        if curso is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = CursoSerializer(curso, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        curso = self.get_object(pk)
+        if curso is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        curso.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class ModuloViewSet(viewsets.ModelViewSet):
     queryset = Modulo.objects.all()
